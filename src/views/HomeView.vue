157382d9a2f32ae3c505a109a2e016ec26010fcc
takumi <zekus1018@gmail.com>
2022-08-26 14:28:35 +0900

    takumiがHOMEを作成しました

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,89 @@
 <template>
   <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <div id="link">
+    <div class="vue_quiz">
+      <router-link to="quiz"
+        ><img src="../image/quiz_img.png" v-on:mouseover="mouse"
+      /></router-link>
+      <h4>Vueクイズ</h4>
+      <h4 v-bind:class="change">作成者：しゅん</h4>
+    </div>
+
+    <div class="vue_palette">
+      <router-link to="palette"
+        ><img src="../image/enogu_img.png" v-on:mouseover="mouse"
+      /></router-link>
+      <h4>Vueパレット</h4>
+      <h4 v-bind:class="change">作成者：しょうちゃん</h4>
+    </div>
+
+    <div class="vue_memo">
+      <router-link to="memo"
+        ><img src="../image/memo_img.png" v-on:mouseover="mouse"
+      /></router-link>
+      <h4>Vueメモ</h4>
+      <h4 v-bind:class="change">作成者：ゆい</h4>
+    </div>
+
+    <router-view></router-view>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      change: "before",
+    }
+  },
+  methods: {
+    mouse: function () {
+      this.change = "after"
+    },
+  },
+}
+</script>
+<style>
+#link {
+  display: flex;
+  justify-content: space-around;
+  margin-right: 40px;
+  height: 400px;
+}
+.vue_quiz {
+  width: 200px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+}
+.vue_palette {
+  width: 200px;
+  height: 200px;
+}
+.vue_memo {
+  width: 200px;
+  height: 200px;
+}
+#link img {
+  width: 250px;
+  height: 250px;
+  background-color: rgb(215, 215, 215);
+  border-radius: 250px;
+}
+h4 {
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 0px;
+}
+.before {
+  opacity: 0;
+}
+.after {
+  opacity: 1;
+  transition: 1s;
+}
+.footer {
+  background-color: rgb(216, 216, 216);
+}
+</style>
